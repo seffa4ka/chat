68225d66c3ab148f08f1e3b0901caeeafa888dc5
seffka <th-seffka@mail.ru>
2021-07-16 23:39:04 +0300

    Change headers

--- index.js
+++ index.js
@@ -67,8 +67,7 @@ passport.use('bearer', new BearerStrategy((token, done) => {
 }));
 app.use(async (ctx, next) => {
     ctx.set('Access-Control-Allow-Origin', '*');
-    ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    ctx.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    ctx.set('Access-Control-Allow-Headers', 'Content-Type');
     await next();
 });
 router.post('/sign-in', async (ctx) => {
